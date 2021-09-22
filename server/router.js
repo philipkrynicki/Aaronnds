726e1996f5a5985c3a230979c5e0084406caee90
Otto Frankel <ottofrankel@comcast.net>
2021-09-22 12:28:34 -0700

    card param

--- server/router.js
+++ server/router.js
@@ -5,6 +5,7 @@ const FakeData = require('./controllers/initial-data');
 const Boards = require('./controllers/boards')
 const Board = require('./models/board');
 const List = require('./models/list');
+const Card = require('./models/card');
 
 app.use(cors());
 
@@ -45,6 +46,23 @@ module.exports = function(app){
         throw err;
       else
         req.list = list;
+      next();
+    })
+  })
+
+  // Fetches a card
+  app.params('card', (req, res, next, id) => {
+    Card.findById(id)
+    .populate({
+      path: 'comments'
+    })
+    .exec((err, card) => {
+      if (!card)
+        req.error = '404';
+      else if (err)
+        throw err;
+      else
+        req.card = card;
       next();
     })
   })
