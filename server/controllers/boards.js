dcd9824373009771ac5cfce9a4c876e64345348c
Heyitstan <heyitstan@gmail.com>
2021-09-22 10:21:04 -0700

    added post board, get list, and post list

--- server/controllers/boards.js
+++ server/controllers/boards.js
@@ -21,3 +21,44 @@ exports.getBoard = (req, res) => {
       res.status(200).json(board)
     })
 }
+
+exports.postBoard = (req, res) => {
+  let board = new Board({
+    name: req.body.name,
+    lists: [],
+    workspace: 'placeholder'
+  });
+  board.save((err, newBoard) => {
+    if (err) return next(err);
+    res.send(newBoard);
+  });
+};
+
+exports.getLists = (req, res) => {
+  List.find({})
+    .exec((err, lists) => {
+      if (err) return next(err);
+      res.status(200).json(lists);
+    });
+};
+
+exports.getList = () => {
+  List.findById(req.params.List)
+  .exec((err, list) => {
+    if(!list) {
+      res.sendStatus(404);
+    }
+    if (err) {
+      next(err);
+    }
+    res.status(200).json(board);
+  });
+};
+
+exports.postList = (req, res) => {
+  let list = new List({
+    name: req.body.name,
+    cards: [],
+    board: {}
+  })
+}
