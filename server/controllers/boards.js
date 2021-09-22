b573813aa557fc41ad69f66ca519e0e3ca2612f3
zdresser <42179244+zdresser@users.noreply.github.com>
2021-09-22 13:07:02 -0400

    Added delete and put routes

---
+++ server/controllers/boards.js
@@ -0,0 +1,49 @@
+const List = require('../models/list');
+const Workspace = require('../models/workspace');
+const Board = require('../models/board');
+
+exports.getBoards = (req, res) => {
+  Board.find({})
+    .exec((err, boards) => {
+      if (err) return next(err);
+      res.status(200).json(boards);
+    })
+}
+
+exports.getBoard = (req, res) => {
+  Board.findById(req.params.board)
+    .exec((err, board) => {
+      if(!board) {
+        res.sendStatus(404)
+      } else if (err) {
+        next(err)
+      }
+      res.status(200).json(board)
+    })
+}
+
+exports.deleteBoard = (req, res) => {
+  Board.deleteOne({_id: req.params.board})
+    .exec((err, board) => {
+      if(!board) {
+        res.sendStatus(404);
+      } else if (err) {
+        next(err)
+      }
+      res.status(200).send("Board deleted")
+    });
+};
+
+exports.putBoard = (req,res) => {
+  const update = req.body;
+
+  Board.findOneAndUpdate({_id: req.params.board}, update, { new: true })
+    .exec((err, updatedBoard) => {
+      if(!board) {
+        res.sendStatus(404);
+      } else if (err) {
+        next(err)
+      }
+      res.status(200).json(updatedBoard)
+    })
+}
--- server/router.js
+++ server/router.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const router = require("express").Router();
 const app = express();
 const cors = require('cors');
+const Boards = require('./controllers/boards')
 //import models
 
 app.use(cors());
 
 
 
-module.exports = router;
+module.exports = function(app){
+  app.get('/api/workspace/boards', Boards.getBoards);
+  app.get('/api/boards/:board', Boards.getBoard);
+  app.delete('/api/boards/:board', Boards.deleteBoard);
+  app.put('/api/boards/:board', Boards.putBoard);
+};
