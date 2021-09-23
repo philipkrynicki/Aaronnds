263868748aacd7e61bfd5aab3c8cb84308707e0e
Otto Frankel <ottofrankel@comcast.net>
2021-09-23 12:22:00 -0700

    deleting multiple cards deletes all referenced comments

--- server/models/card.js
+++ server/models/card.js
@@ -10,4 +10,15 @@ const cardSchema = new Schema({
   list: {type: Schema.Types.ObjectId, ref: 'List'},
 })
 
+cardSchema.pre('deleteMany', {document: false, query: true}, async function(next) {
+  const cards = await this.model.find(this.getFilter());
+
+  const ids = cards.map(card => {
+    return card._id;
+  })
+
+  // Delete all cards that reference any of the ids
+  cards[0].model('Comment').deleteMany({card: {'$in': ids}}, next);
+})
+
 module.exports = mongoose.model('Card', cardSchema);
