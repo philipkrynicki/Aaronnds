90b8af1d0d3e6c06d9b0770061fe3c524b8c3136
Otto Frankel <ottofrankel@comcast.net>
2021-09-23 11:54:53 -0700

    deleting multiple lists deletes all referenced cards

--- server/models/list.js
+++ server/models/list.js
@@ -7,4 +7,16 @@ const listSchema = new Schema({
   board: {type: Schema.Types.ObjectId, ref: 'Board'}
 })
 
+listSchema.pre('deleteMany', {document: false, query: true}, async function(next) {
+  const lists = await this.model.find(this.getFilter());
+
+  // Get the list ids
+  const ids = lists.map(list => {
+    return list._id;
+  })
+
+  // Delete all cards that reference any of the ids
+  lists[0].model('Card').deleteMany({list: {'$in': ids}}, next);
+})
+
 module.exports = mongoose.model('List', listSchema);
