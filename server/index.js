ccaca906cf3b6b58e97352064d9ce99a0f1da95a
Otto Frankel <ottofrankel@comcast.net>
2021-09-27 10:47:39 -0700

    npm install

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require('./router');
 const cors = require('cors');
-const passport = require("passport");
+const passport = require('passport');
 
 mongoose.connect("mongodb://localhost/aaronnds");
 
