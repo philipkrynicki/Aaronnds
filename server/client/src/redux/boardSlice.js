c212ca781fbf8d981b936a03c0ddad02dfbaba8f
Otto Frankel <ottofrankel@comcast.net>
2021-09-27 10:19:18 -0700

    Frontend connected to backend via sockets

--- server/client/src/redux/boardSlice.js
+++ server/client/src/redux/boardSlice.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { io } from 'socket.io-client';
+
+// Connect to the socket on the backend server
+const socket = io('http://localhost:5000', {transports: ['websocket']});
 
 export const getBoardAsync = createAsyncThunk(
   'board/getBoardAsync',
