e111ddd9b4cd63e95d387e8040fdec0911881f33
Otto Frankel <ottofrankel@comcast.net>
2021-09-29 12:20:45 -0700

    addBoardAsync now uses getPostData

--- server/client/src/util-functions/get-response-data.js
+++ server/client/src/util-functions/get-response-data.js
@@ -6,9 +6,16 @@ const getPostData = async (url, body) => {
   // Data from a socket
   if (body.hasOwnProperty('_id'))
     return body;
+
+  let reqBody = {};
+
+  if (body.hasOwnProperty('nameObj'))
+    reqBody = body.nameObj
+  else
+    reqBody = body;
   
   // Data from an http request
-  const response = await axios.post(url, body.nameObj);
+  const response = await axios.post(url, reqBody);
   return response.data;
 }
 
