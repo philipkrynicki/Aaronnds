c415c0a8320d20f46162c1ead40ec6d68532d134
Heyitstan <heyitstan@gmail.com>
2021-09-28 20:29:13 -0700

    http const

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const socket = require('socket.io');
-
+const http = require('http')
 const router = require('./router');
 const passport = require("passport");
 
