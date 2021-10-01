3445c6edc3b4792309a9201041cb00a911647875
Benjamin Pryor <bscott23@gmail.com>
2021-10-01 17:32:48 -0400

    changed title and icon

--- server/client/src/redux/listSlice.js
+++ server/client/src/redux/listSlice.js
@@ -77,7 +77,6 @@ export const addCardAsync = createAsyncThunk(
     'cards/moveCardAsync',
     async (card) => {
       const data = await getResponseData(`${apiUrl}/lists/${card.list}/cards/${card.id}`, card.destList, 'PUT')
-      console.log(data)
       store.dispatch(getListsAsync(data.updatedList.board))
       const today = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short'});
       const now = new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
